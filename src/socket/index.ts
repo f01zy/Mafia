853adc3bfb5ab4add4e5183afd92f738700415ed
f01zy <aminovali015@icloud.com>
2025-07-27 03:50:31 +0300

    add events

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,37 @@
 import type { Server } from "socket.io";
 import { logger } from "../utils/logger.utils";
 
+type User = {
+  socket: string,
+  username: string
+}
+
+type Room = {
+  owner: User,
+  name: string,
+  maxPlayers: number,
+  players: Array<User>
+}
+
+const rooms: Array<Room> = [];
+
 export const loadIoListeners = (io: Server) => {
   io.on("connection", (socket) => {
     logger.info(`User ${socket.id} was connected`)
+
+    socket.on("getRooms", () => {
+      const names = rooms.map(room => room.name)
+      socket.emit("receiveRooms", JSON.stringify({ rooms: names }))
+    })
+
+    socket.on("createRoom", (s: string) => {
+      const data: Omit<Room, "players"> = JSON.parse(s);
+      const room: Room = {
+        ...data,
+        players: [data.owner]
+      }
+      rooms.push(room)
+      socket.emit("receiveCreateRoom", JSON.stringify(room))
+    })
   })
 }
