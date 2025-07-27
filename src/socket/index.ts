44d0684ac6a04c8d557c5df74b3d64dfd72b72dc
f01zy <aminovali015@icloud.com>
2025-07-28 02:28:48 +0300

    complete rooms

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,37 +1,94 @@
-import type { Server } from "socket.io";
+import type { Server, Socket } from "socket.io";
 import { logger } from "../utils/logger.utils";
 
-type User = {
-  socket: string,
-  username: string
-}
-
 type Room = {
-  owner: User,
+  owner: string,
   name: string,
   maxPlayers: number,
-  players: Array<User>
+  players: Array<string>
 }
 
 const rooms: Array<Room> = [];
 
 export const loadIoListeners = (io: Server) => {
-  io.on("connection", (socket) => {
-    logger.info(`User ${socket.id} was connected`)
+  const disconnectFromRoom = (socket: Socket) => {
+    const roomName = socket.data.room
+    const username = socket.data.username
+    const i = rooms.findIndex(r => r.name == roomName)
 
-    socket.on("getRooms", () => {
-      const names = rooms.map(room => room.name)
-      socket.emit("receiveRooms", JSON.stringify({ rooms: names }))
+    if (i != -1) {
+      const room = rooms[i]
+      room.players = room.players.filter(player => player != username)
+
+      socket.data.room = null
+      socket.leave(roomName)
+
+      if (room.players.length == 0) {
+        rooms.splice(i, 1)
+        logger.info(`The room ${room.name} removed`)
+      }
+
+      else {
+        if (room.owner == username) {
+          room.owner = room.players[0]
+        }
+        io.to(room.name).emit("updateRoom", JSON.stringify(room))
+      }
+
+      logger.info(`${username} disconnected from ${roomName}`)
+      socket.emit("receiveDisconnectFromRoom")
+    }
+  }
+
+  io.on("connection", (socket) => {
+    socket.on("authenticate", (username: string) => {
+      socket.data.username = username
+      logger.info(`${username} connected`)
     })
 
-    socket.on("createRoom", (s: string) => {
-      const data: Omit<Room, "players"> = JSON.parse(s);
+    socket.on("getRooms", () => {
+      socket.emit("receiveRooms", JSON.stringify({ rooms }))
+    })
+
+    socket.on("createRoom", (json: string) => {
+      const data: Omit<Room, "players" | "owner"> = JSON.parse(json);
+      const username = socket.data.username;
+
       const room: Room = {
         ...data,
-        players: [data.owner]
+        owner: username,
+        players: [username]
       }
+
       rooms.push(room)
-      socket.emit("receiveCreateRoom", JSON.stringify(room))
+      socket.join(data.name)
+      socket.data.room = data.name
+
+      logger.info(`The room ${room.name} created`)
+      socket.emit("updateRoom", JSON.stringify(room))
+    })
+
+    socket.on("disconnectFromRoom", () => {
+      disconnectFromRoom(socket)
+    })
+
+    socket.on("connectToRoom", (roomName: string) => {
+      const room = rooms.find(r => r.name == roomName)
+      const username = socket.data.username
+
+      if (!room) {
+        return
+      }
+      room.players.push(username)
+
+      logger.info(`${username} connected to ${roomName}`)
+      socket.data.room = roomName
+      socket.join(roomName)
+      io.to(roomName).emit("updateRoom", JSON.stringify(room))
+    })
+
+    socket.on("disconnect", () => {
+      disconnectFromRoom(socket)
     })
   })
 }
