3980d013ab6dec8e26491bf3763a6b24a6abbbba
f01zy <aminovali015@icloud.com>
2025-07-30 01:12:34 +0300

    Add start game handler (no work) and edit all scenes

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -12,30 +12,30 @@ const rooms: Array<Room> = [];
 
 export const loadIoListeners = (io: Server) => {
   const disconnectFromRoom = (socket: Socket) => {
-    const roomName = socket.data.room
+    const currentRoom = socket.data.currentRoom
     const username = socket.data.username
-    const i = rooms.findIndex(r => r.name == roomName)
+    const i = rooms.findIndex(room => room.name == currentRoom)
 
     if (i != -1) {
       const room = rooms[i]
       room.players = room.players.filter(player => player != username)
 
       socket.data.room = null
-      socket.leave(roomName)
+      socket.leave(currentRoom)
 
       if (room.players.length == 0) {
         rooms.splice(i, 1)
-        logger.info(`The room ${room.name} removed`)
+        logger.info(`The room ${currentRoom} removed`)
       }
 
       else {
         if (room.owner == username) {
           room.owner = room.players[0]
         }
-        io.to(room.name).emit("updateRoom", JSON.stringify(room))
+        io.to(currentRoom).emit("updateRoom", JSON.stringify(room))
       }
 
-      logger.info(`${username} disconnected from ${roomName}`)
+      logger.info(`${username} disconnected from ${currentRoom}`)
       socket.emit("receiveDisconnectFromRoom")
     }
   }
@@ -62,7 +62,7 @@ export const loadIoListeners = (io: Server) => {
 
       rooms.push(room)
       socket.join(data.name)
-      socket.data.room = data.name
+      socket.data.currentRoom = data.name
 
       logger.info(`The room ${room.name} created`)
       socket.emit("updateRoom", JSON.stringify(room))
@@ -72,8 +72,8 @@ export const loadIoListeners = (io: Server) => {
       disconnectFromRoom(socket)
     })
 
-    socket.on("connectToRoom", (roomName: string) => {
-      const room = rooms.find(r => r.name == roomName)
+    socket.on("connectToRoom", (name: string) => {
+      const room = rooms.find(r => r.name == name)
       const username = socket.data.username
 
       if (!room) {
@@ -85,10 +85,21 @@ export const loadIoListeners = (io: Server) => {
       }
       room.players.push(username)
 
-      logger.info(`${username} connected to ${roomName}`)
-      socket.data.room = roomName
-      socket.join(roomName)
-      io.to(roomName).emit("updateRoom", JSON.stringify(room))
+      logger.info(`${username} connected to ${name}`)
+      socket.data.currentRoom = name
+      socket.join(name)
+      io.to(name).emit("updateRoom", JSON.stringify(room))
+    })
+
+    socket.on("startGame", () => {
+      const currentRoom = socket.data.room
+
+      const i = rooms.findIndex(r => r.name == currentRoom)
+      if (i != -1) {
+        rooms.splice(i, 1)
+      }
+
+      io.to(currentRoom).emit("receiveStartGame")
     })
 
     socket.on("disconnect", () => {
